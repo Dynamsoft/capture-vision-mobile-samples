05632086c5d8b94479c44502d28182da3d84f08d
Allen <Allen>
2024-04-17 17:32:23 +0800

    DriverLicese sample update to dcv 2.2.10

--- Android/DriversLicenseScanner/app/src/main/java/com/dynamsoft/dcv/driverslicensescanner/fragments/ScannerFragment.java
+++ Android/DriversLicenseScanner/app/src/main/java/com/dynamsoft/dcv/driverslicensescanner/fragments/ScannerFragment.java
@@ -10,7 +10,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.dynamsoft.core.basic_structures.CapturedResultReceiver;
+import com.dynamsoft.cvr.CapturedResultReceiver;
 import com.dynamsoft.cvr.CaptureVisionRouter;
 import com.dynamsoft.cvr.CaptureVisionRouterException;
 import com.dynamsoft.dbr.DecodedBarcodesResult;
